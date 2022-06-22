dfe6a275e81033b67f65b7a5992f8eef06b8b479
imskanand <the.anand3099@gmail.com>
2022-06-22 18:27:10 +0530

    ++ Added the assets folder Added the required hooks to make our navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
+
+const Navbar = () => (
+  <div>Navbar</div>
+);
+export default Navbar;
