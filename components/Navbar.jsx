de183aa9e5966e9374df3fddc113e265d23b8ebf
imskanand <the.anand3099@gmail.com>
2022-06-22 19:37:08 +0530

    Change and added the logo and Dark or Light bar toggel

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,35 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   console.log({ theme });
   return (
-    <div>Navbar</div>
+    <nav className="fixed z-10 flex-row w-full p-4 bg-white border-b flexBetween dark:bg-nft-dark dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-row justify-start flex-1">
+        <Link href="/">
+          <div className="cursor-pointer flexCenter md:hidden">
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="ml-1 text-lg font-semibold dark:text-white text-nft-black-1">CryptoKet</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden cursor-pointer md:flex">
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-row justify-end flex-initial">
+        <div className="flex mr-2 item-center">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => {}}
+          />
+          <label htmlFor="checkbox" className="relative w-8 h-4 p-1 bg-black flexBetween rounded-2xl label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+          </label>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Navbar;
